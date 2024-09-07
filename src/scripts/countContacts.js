03aae55a491c6bf1d8f5b58557f5beda0071581f
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-07 09:37:46 +0300

    upd countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    return contacts.length;
+  } catch (error) {
+    console.error('Error counting contacts: ', error);
+    throw error;
+  }
+};
 
 console.log(await countContacts());
