b24004dd6a971e8460eb408a926a81113460bcda
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-07 09:41:29 +0300

    upd removeLastContact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    contacts.pop();
+    await writeContacts(contacts);
+    console.log('Last contact remove successfull!');
+  } catch (error) {
+    console.log('Failed to delete contact from the file: ', error);
+  }
+};
 
 removeLastContact();
