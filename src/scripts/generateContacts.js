050513fbca5b0dd72f77b17125be21505da22e1f
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-07 07:55:16 +0300

    upd generate contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    for (let index = 0; index < number; index++) {
+      contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+    console.log('Contacts added succefull!');
+  } catch (error) {
+    console.log('failed to read contacts from the file:', error);
+  }
+};
 
 generateContacts(5);
